3ff5805a46ab75456b52126023dfb8856ef7df5d
Luis Prada Conde <luis.prada@udc.es>
2021-04-25 15:42:30 +0200

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -3,34 +3,12 @@ cd $(dirname $0)
 
 cd ../complete
 
-mvn clean package
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf target
-
-./gradlew build
+./gradlew test --rerun-tasks --stacktrace
 ret=$?
 if [ $ret -ne 0 ]; then
 exit $ret
 fi
 rm -rf build
 
-cd ../initial
-
-mvn clean compile
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf target
-
-./gradlew compileJava
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf build
 
 exit
